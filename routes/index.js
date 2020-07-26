166b2039bebcadba4c2d5866a491f86af9c18d51
Phiiileo <emmanuelboluwajib1@gmail.com>
2020-07-26 04:08:59 +0100

    show image in index page

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,23 @@
 const router = require('express').Router();
+const Book = require('./../model/book')
+const Author = require('./../model/author');
 
 
-router.get('/', (req, res) => {
-    res.render('index')
+router.get('/', async (req, res) => {
+    try {
+        const books = await Book.find({})
+        const authors = await Author.find({})
+        res.render('index', {
+            books,
+            authors
+        })
+    } catch (err) {
+        console.log(err)
+        res.render('index', {
+            books: [],
+            authors: []
+        })
+    }
 })
 
 module.exports = router
